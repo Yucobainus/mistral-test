92b294119e932a1ca6fe2fc3477abf55ac4bc9f6
Yucobainus <naibrgood@gmail.com>
2022-01-18 02:48:48 +0300

    Adaptation for header

---
+++ src/js/script.js
@@ -0,0 +1,16 @@
+const iconMenu = document.querySelector('.menu-icon')
+const menuBody = document.querySelector('.mobile-header')
+if (iconMenu) {
+    iconMenu.addEventListener('click', function (e) {
+        iconMenu.classList.toggle('_active')
+        menuBody.classList.toggle('_active')
+    })
+}
+
+
+const mobileItem = document.querySelector('.mobile');
+
+mobileItem.addEventListener('click', function (e) {
+    iconMenu.classList.remove('_active')
+    menuBody.classList.remove('_active')
+})
